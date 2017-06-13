744c4260a66d52d89add33adbc858f5253d2798a
Chaewon Min <mcw0805@gmail.com>
2017-06-13 13:19:41 -0400

    set up project

---
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.mcw0805.wheres_my_stuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
