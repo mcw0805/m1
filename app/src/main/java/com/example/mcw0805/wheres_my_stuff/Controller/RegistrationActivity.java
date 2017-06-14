82c5ee07f6ad189f3c1a8cc73baa1d0a6aace6a3
Jordan <jordan.taylor@gatech.edu>
2017-06-13 20:29:04 -0400

    Added Registration UI

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/RegistrationActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/RegistrationActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.mcw0805.wheres_my_stuff.R;
 
-public class ProfileActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_registration);
     }
 }
