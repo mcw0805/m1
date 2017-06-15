e0a607a0083e0c1b03d7cc6cc7dfd8b84bb04689
Melanie Hall <swimdawg@live.com>
2017-06-15 14:52:04 -0400

    Added log out button.

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/ProfileActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/ProfileActivity.java
@@ -1,15 +1,34 @@
 package com.example.mcw0805.wheres_my_stuff.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.example.mcw0805.wheres_my_stuff.R;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    //Log out button.
+    private Button logoutProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logoutProfile = (Button) findViewById(R.id.profile_logout_btn);
+
+        logoutProfile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == logoutProfile) {
+            Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
+            ProfileActivity.this.startActivity(intent);
+        }
     }
 }
