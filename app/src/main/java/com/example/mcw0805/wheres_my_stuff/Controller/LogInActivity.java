977940ab010442e46cfa0d36f99fd927c9dc2b33
TeddyShang <teddyshang99@gmail.com>
2017-06-15 14:26:40 -0400

    Editted LogInActivity.java for button implementations

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/LogInActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/LogInActivity.java
@@ -15,53 +15,53 @@ import com.google.firebase.auth.FirebaseAuth;
  * @Author Ted Shang
  * @Version 1.0
  */
-public class LogInActivity extends AppCompatActivity  {
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
     //We will be getting information from the following widgets:
 
     private EditText loginUsername;
     private EditText loginPassword;
+    private Button login;
+    private Button back;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        loginUsername =(EditText) findViewById(R.id.login_username_edit);
-        loginPassword =(EditText) findViewById(R.id.login_pw_edit);
+        loginUsername = (EditText) findViewById(R.id.login_username_edit);
+        loginPassword = (EditText) findViewById(R.id.login_pw_edit);
 
-        //Create the buttons
-        Button logIn = (Button) findViewById(R.id.login_btn);
-        Button back = (Button) findViewById(R.id.login_back_btn);
-
-        logIn.setOnClickListener(new View.OnClickListener() {
-            //Gets the information from the EditTexts and checks it against a username/password
-            //database. Either displays incorrect password or advances the scene
-            public void onClick(View v) {
-                String username = loginUsername.getText().toString();
-                String password = loginPassword.getText().toString();
-                //Temporary hardcode
-                String hardUsername = "user";
-                String hardPassword = "pass";
-                if (username.equals(hardUsername) && password.equals(hardPassword)) {
-                    //GOTO application
-                    Intent intent = new Intent(LogInActivity.this, ProfileActivity.class);
-                    LogInActivity.this.startActivity(intent);
-                } else {
-                    // Inform user of bad login attempt
-                }
-            }
-        });
-
-        back.setOnClickListener(new View.OnClickListener() {
-            //Goes back to the main splash screen
-            public void onClick(View V) {
-                Intent intent = new Intent (LogInActivity.this, HomeActivity.class);
-                LogInActivity.this.startActivity(intent);
-            }
-        });
-
-
+        //set the buttons
+        login = (Button) findViewById(R.id.login_btn);
+        back = (Button) findViewById(R.id.login_back_btn);
+        login.setOnClickListener(this);
+        back.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == login) {
+            //Gets the information from the EditTexts and checks it against a username/password
+            //database. Either displays incorrect password or advances the scene
+            String username = loginUsername.getText().toString();
+            String password = loginPassword.getText().toString();
+            //Temporary hardcode
+            String hardUsername = "user";
+            String hardPassword = "pass";
+            if (username.equals(hardUsername) && password.equals(hardPassword)) {
+                //GOTO application
+                Intent intent = new Intent(LogInActivity.this, ProfileActivity.class);
+                LogInActivity.this.startActivity(intent);
+            } else {
+                // Inform user of bad login attempt
+            }
+        }
+        if (v == back) {
+            //Goes back to the main splash screen
+            Intent intent = new Intent (LogInActivity.this, HomeActivity.class);
+            LogInActivity.this.startActivity(intent);
+        }
+
+    }
 }
