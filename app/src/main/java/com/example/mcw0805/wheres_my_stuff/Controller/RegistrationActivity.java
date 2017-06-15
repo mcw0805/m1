d6e6f4d6d088dd6e8597ccd21012fdcf3b464aa4
Chaewon Min <mcw0805@gmail.com>
2017-06-15 14:19:34 -0400

    Updating registration activity

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/RegistrationActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/RegistrationActivity.java
@@ -1,15 +1,32 @@
 package com.example.mcw0805.wheres_my_stuff.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mcw0805.wheres_my_stuff.R;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button cancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        cancelButton = (Button) findViewById(R.id.registrationCancel);
+
+        cancelButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == cancelButton) {
+            finish();
+            startActivity(new Intent(this, HomeActivity.class));
+        }
+
     }
 }
