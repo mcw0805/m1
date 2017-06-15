9b4b9ca3ec0d54f4d84c6c9fbe2ffc6edeaeee15
connelly7 <chiannerose@gmail.com>
2017-06-15 14:12:14 -0400

    Added Buttons to controller
    Added the home activity buttons to the controller so the view and controller are connected

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/HomeActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/HomeActivity.java
@@ -2,14 +2,24 @@ package com.example.mcw0805.wheres_my_stuff.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.mcw0805.wheres_my_stuff.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    //create the buttons on the home page
+    private Button loginHome;
+    private Button registerHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        loginHome = (Button) findViewById(R.id.loginHome);
+        registerHome = (Button) findViewById(R.id.registerHome);
+
     }
+
 }
