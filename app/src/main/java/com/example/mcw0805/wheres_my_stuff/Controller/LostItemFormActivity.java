67ea3ba8d8001681f8f91d66337db2f333d75fae
melaniehall12 <swimdawg@live.com>
2017-06-19 09:08:25 -0400

    Added lost item form page.

---
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/LostItemFormActivity.java
@@ -0,0 +1,15 @@
+package com.example.mcw0805.wheres_my_stuff.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mcw0805.wheres_my_stuff.R;
+
+public class LostItemFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_item_form);
+    }
+}
