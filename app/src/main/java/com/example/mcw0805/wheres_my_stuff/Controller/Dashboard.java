86c4c4d1682821add5891c8202a1dd4e8eaaec47
connelly7 <chiannerose@gmail.com>
2017-06-20 15:57:08 -0400

    Added the import for the Intent
    ^^

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/Dashboard.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/Dashboard.java
@@ -1,5 +1,6 @@
 package com.example.mcw0805.wheres_my_stuff.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
