53a4d960775c44ebe28f4afd95c40a8a38f20849
Jordan <jordan.taylor@gatech.edu>
2017-06-21 17:13:34 -0400

    Edited Reistration

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/RegistrationActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/RegistrationActivity.java
@@ -1,25 +1,38 @@
 package com.example.mcw0805.wheres_my_stuff.Controller;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.mcw0805.wheres_my_stuff.R;
+import com.google.firebase.auth.FirebaseAuth;
 
+/**
+ * @Author Jordan Taylor
+ * @Version 1.0
+ */
 public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button cancelButton, loginButton;
     private EditText name, email, password;
     private Switch admin;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
+        mAuth = FirebaseAuth.getInstance();
         cancelButton = (Button) findViewById(R.id.registrationCancel);
         cancelButton.setOnClickListener(this);
 
@@ -30,32 +43,42 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         email = (EditText) findViewById(R.id.registrationEmail);
         password = (EditText) findViewById(R.id.registrationPassword);
         admin = (Switch) findViewById(R.id.registrationAdmin);
-//
-//        try {
-//
-//        } catch
-
     }
 
     @Override
     public void onClick(View v) {
-        int i = 0;
-        if (v == cancelButton) {
-            i = 1;
-        } else if (v == loginButton) {
-            i = 2;
+
+        String inputName = name.getText().toString();
+        String inputEmail = email.getText().toString();
+        String inputPassword = password.getText().toString();
+        boolean inputAdmin = admin.isChecked();
+
+        if (v == loginButton) {
+            if (inputEmail.length() == 0 || inputName.length() == 0 ||
+                    inputPassword.length() == 0) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+                builder1.setMessage("Please fill out all required fields.");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert = builder1.create();
+                alert.show();
+                return;
+            }
+            finish();
+            startActivity(new Intent(this, HomeActivity.class));
         }
-        switch (i) {
-            case 1:
-                finish();
-                startActivity(new Intent(this, HomeActivity.class));
-                break;
-            case 2:
-                finish();
-                startActivity(new Intent(this, ProfileActivity.class));
-                break;
-            default:
-                break;
+
+        if (v == cancelButton) {
+            finish();
+            startActivity(new Intent(this, HomeActivity.class));
         }
     }
 
