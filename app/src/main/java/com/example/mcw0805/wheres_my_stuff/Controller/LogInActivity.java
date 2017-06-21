ba464a58cd717e7593d62d5e548ecd6175d39843
Chaewon Min <mcw0805@gmail.com>
2017-06-21 19:51:18 -0400

    Moved login functionality to a private method

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/LogInActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/LogInActivity.java
@@ -83,52 +83,66 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
 
         if (v == login) {
-            //Gets the information from the EditTexts and checks it against a username/password
-            //database. Either displays incorrect password or advances the scene
-            String username = loginUsername.getText().toString();
-            String password = loginPassword.getText().toString();
-            //check to see if fields are valid first;
-            if (username.length() == 0 || password.length() == 0) {
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-                builder1.setMessage("Please fill out both required fields.");
-                builder1.setCancelable(true);
+           login();
 
-                builder1.setPositiveButton(
-                        "Cancel",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
+        }
 
-                builder1.setNegativeButton(
-                        "Ok",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
+        if (v == back) {
+            //Goes back to the main splash screen
+            finish();
+            Intent intent = new Intent (LogInActivity.this, HomeActivity.class);
+            LogInActivity.this.startActivity(intent);
+        }
 
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-                return;
-            }
-            mAuth.signInWithEmailAndPassword(username, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d(TAG, "signInWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Intent intent = new Intent(LogInActivity.this, Dashboard.class);
-                                LogInActivity.this.startActivity(intent);
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                Toast.makeText(LogInActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
+    }
+
+    private void login() {
+        //Gets the information from the EditTexts and checks it against a username/password
+        //database. Either displays incorrect password or advances the scene
+        String username = loginUsername.getText().toString();
+        String password = loginPassword.getText().toString();
+        //check to see if fields are valid first;
+        if (username.length() == 0 || password.length() == 0) {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("Please fill out both required fields.");
+            builder1.setCancelable(true);
+
+            builder1.setPositiveButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            builder1.setNegativeButton(
+                    "Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LogInActivity.this, Dashboard.class);
+                            LogInActivity.this.startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                             /*Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
                             // If sign in fails, display a message to the user. If sign in succeeds
                             // the auth state listener will be notified and logic to handle the
@@ -142,8 +156,8 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                             LogInActivity.this.startActivity(intent);
 
                             // ...*/
-                        }
-                    });
+                    }
+                });
             /*//Temporary hardcode
             String hardUsername = "user";
             String hardPassword = "pass";
@@ -178,14 +192,6 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                 AlertDialog alert11 = builder1.create();
                 alert11.show();*/
 
-
-            }
-        if (v == back) {
-            //Goes back to the main splash screen
-            finish();
-            Intent intent = new Intent (LogInActivity.this, HomeActivity.class);
-            LogInActivity.this.startActivity(intent);
-        }
 
     }
 }
