d6e66f38fb29ba9f461aa4d9437810f7ee0d0a51
TeddyShang <teddyshang99@gmail.com>
2017-06-27 12:15:08 -0400

    Merge branch 'master' of https://github.com/mcw0805/wheres-my-stuff
    # Conflicts:
    #	app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/RegistrationActivity.java
    #	app/src/main/java/com/example/mcw0805/wheres_my_stuff/Model/User.java

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Model/User.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Model/User.java
@@ -18,6 +18,7 @@ public class User {
      *
      * @param name  name of user
      * @param email email of user
+     * @param id id of the user
      */
     public User(String name, String email, String id) {
         this.name = name;
@@ -29,6 +30,15 @@ public class User {
 
     }
 
+    /**
+     * Constructor provided with entered info from user
+     * @param name name of user
+     * @param email email of user
+     * @param id uid of user
+     * @param isLocked locked status of user
+     * @param isBanned banned status of user
+     */
+
     public User(String name, String email, String id, boolean isLocked, boolean isBanned) {
         this.name = name;
         this.email = email;
@@ -38,42 +48,82 @@ public class User {
 
     }
 
+    /**
+     * gets name
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * sets the name of the user
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * gets the email of the user
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * sets the email of the user
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * gets the locked status of the yser
+     * @return bool true or false
+     */
     public boolean isLocked() {
         return isLocked;
     }
 
+    /**
+     * sets the locked status of the user
+     * @param locked
+     */
     public void setLocked(boolean locked) {
         this.isLocked = locked;
     }
 
+    /**
+     * gets the banned status of the user
+     * @return bool t/f
+     */
     public boolean isBanned() {
         return isBanned;
     }
 
+    /**
+     * sets the banned status of the user
+     * @param banned
+     */
     public void setBanned(boolean banned) {
         this.isBanned = banned;
     }
 
+    /**
+     * gets the uid of the user
+     * @return uid
+     */
     public String getUid() {
         return uid;
     }
 
+    /**
+     * sets the uid of the user
+     * @param uid
+     */
     public void setUid(String uid) {
         this.uid = uid;
     }
