4a47417d5dc0bf475a32afc82497db31d07311a0
TeddyShang <teddyshang99@gmail.com>
2017-06-29 22:25:46 -0400

    Bugfixes in LoginActivity.java, reimplemented admin login

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/LogInActivity.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/LogInActivity.java
@@ -16,6 +16,7 @@ package com.example.mcw0805.wheres_my_stuff.Controller;
         import android.widget.Toast;
 
 
+        import com.example.mcw0805.wheres_my_stuff.Model.Admin;
         import com.example.mcw0805.wheres_my_stuff.Model.User;
         import com.example.mcw0805.wheres_my_stuff.R;
         import com.google.android.gms.tasks.OnCompleteListener;
@@ -28,12 +29,13 @@ package com.example.mcw0805.wheres_my_stuff.Controller;
         import com.google.firebase.database.DatabaseError;
         import com.google.firebase.database.DatabaseReference;
         import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
 
 /**
  * Controller for the login. Uses Firebase email password authentication.
  *
  * @Author Ted Shang
- * @Version 1.0
+ * @Version 1.3
  */
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -162,104 +164,91 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                             final String uid = user.getUid();
                             mUserRef = FirebaseDatabase.getInstance().getReference("users");
                             mUserRef2 = mUserRef.child(uid);
-                            mUserRef.addChildEventListener(new ChildEventListener() {
+                            mUserRef.addValueEventListener(new ValueEventListener() {
                                 @Override
-                                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                                    if (dataSnapshot.getKey().equals(uid)) {
-                                        User u = null;
-                                        try {
-                                            u = User.buildUserObject(dataSnapshot);
-                                        } catch (NullPointerException e) {
-                                            Log.d(TAG, "NullPointerException is caught.");
-                                            e.printStackTrace();
-                                        }
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    for (DataSnapshot user : dataSnapshot.getChildren()) {
+                                        User u = User.buildUserObject(user);
+                                        if (u!= null) {
+                                            if (u.getUid().equals(uid)) {
+                                                if (u.getIsBanned()) {
+                                                    //If banned ALERT
+                                                    AlertDialog.Builder builder1 = new AlertDialog.Builder(LogInActivity.this);
+                                                    builder1.setMessage("Your account has been BANNED for violating ToS. Please contact support ");
+                                                    builder1.setCancelable(true);
 
-                                        //checks if user isBanned
-                                        if (u.getIsBanned()) {
-                                            //If banned ALERT
-                                            AlertDialog.Builder builder1 = new AlertDialog.Builder(LogInActivity.this);
-                                            builder1.setMessage("Your account has been BANNED for violating ToS. Please contact support ");
-                                            builder1.setCancelable(true);
+                                                    builder1.setPositiveButton(
+                                                            "Cancel",
+                                                            new DialogInterface.OnClickListener() {
+                                                                public void onClick(DialogInterface dialog, int id) {
+                                                                    dialog.cancel();
+                                                                    finish();
+                                                                    startActivity(getIntent());
+                                                                }
+                                                            });
 
-                                            builder1.setPositiveButton(
-                                                    "Cancel",
-                                                    new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int id) {
-                                                            dialog.cancel();
-                                                            finish();
-                                                            startActivity(getIntent());
-                                                        }
-                                                    });
+                                                    builder1.setNegativeButton(
+                                                            "Ok",
+                                                            new DialogInterface.OnClickListener() {
+                                                                public void onClick(DialogInterface dialog, int id) {
+                                                                    dialog.cancel();
+                                                                    finish();
+                                                                    startActivity(getIntent());
+                                                                }
+                                                            });
 
-                                            builder1.setNegativeButton(
-                                                    "Ok",
-                                                    new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int id) {
-                                                            dialog.cancel();
-                                                            finish();
-                                                            startActivity(getIntent());
-                                                        }
-                                                    });
+                                                    AlertDialog alert11 = builder1.create();
+                                                    alert11.show();
+                                                    return;
+                                                }
+                                                //Checks if user is locked out
+                                                else if (u.getLockAttempts() > 2) {
+                                                    //locked out set value to true
+                                                    DatabaseReference mLockedout = mUserRef2.child("locked");
+                                                    mLockedout.setValue(true);
+                                                    //if locked out ALERT
+                                                    AlertDialog.Builder builder1 = new AlertDialog.Builder(LogInActivity.this);
+                                                    builder1.setMessage("Your account has been locked from too many incorrect logins. Please try later");
+                                                    builder1.setCancelable(true);
 
-                                            AlertDialog alert11 = builder1.create();
-                                            alert11.show();
-                                        }
-                                        //Checks if user is locked out
-                                        else if (u.getLockAttempts() > 2) {
-                                            //locked out set value to true
-                                            DatabaseReference mLockedout = mUserRef2.child("locked");
-                                            mLockedout.setValue(true);
-                                            //if locked out ALERT
-                                            AlertDialog.Builder builder1 = new AlertDialog.Builder(LogInActivity.this);
-                                            builder1.setMessage("Your account has been locked from too many incorrect logins. Please try later");
-                                            builder1.setCancelable(true);
+                                                    builder1.setPositiveButton(
+                                                            "Cancel",
+                                                            new DialogInterface.OnClickListener() {
+                                                                public void onClick(DialogInterface dialog, int id) {
+                                                                    dialog.cancel();
+                                                                    finish();
+                                                                    startActivity(getIntent());
+                                                                }
+                                                            });
 
-                                            builder1.setPositiveButton(
-                                                    "Cancel",
-                                                    new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int id) {
-                                                            dialog.cancel();
-                                                            finish();
-                                                            startActivity(getIntent());
-                                                        }
-                                                    });
+                                                    builder1.setNegativeButton(
+                                                            "Ok",
+                                                            new DialogInterface.OnClickListener() {
+                                                                public void onClick(DialogInterface dialog, int id) {
+                                                                    dialog.cancel();
+                                                                    finish();
+                                                                    startActivity(getIntent());
+                                                                }
+                                                            });
 
-                                            builder1.setNegativeButton(
-                                                    "Ok",
-                                                    new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int id) {
-                                                            dialog.cancel();
-                                                            finish();
-                                                            startActivity(getIntent());
-                                                        }
-                                                    });
-
-                                            AlertDialog alert11 = builder1.create();
-                                            alert11.show();
-                                        } else {
-                                            //Everything is fine
-                                            //Reset login attempts
-                                            DatabaseReference mLoginAttempts = mUserRef2.child("lockAttempts");
-                                            mLoginAttempts.setValue(Integer.valueOf(0));
-                                            //advance to next screen
-                                            Intent intent = new Intent(LogInActivity.this, Dashboard.class);
-                                            LogInActivity.this.startActivity(intent);
+                                                    AlertDialog alert11 = builder1.create();
+                                                    alert11.show();
+                                                    return;
+                                                }
+                                                //Everything is fine
+                                                //Reset login attempts
+                                                DatabaseReference mLoginAttempts = mUserRef2.child("lockAttempts");
+                                                mLoginAttempts.setValue(Integer.valueOf(0));
+                                                //advance to next screen
+                                                Intent intent = new Intent(LogInActivity.this, Dashboard.class);
+                                                LogInActivity.this.startActivity(intent);
+                                                return;
+                                            }
                                         }
                                     }
-                                }
-
-                                @Override
-                                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                                }
-
-                                @Override
-                                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                                }
-
-                                @Override
-                                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                                    //otherwise we have an admin and can proceed to AdminDashboard
+                                    Intent intent = new Intent(LogInActivity.this, AdminDashboard.class);
+                                    LogInActivity.this.startActivity(intent);
 
                                 }
 
@@ -268,7 +257,6 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
                                 }
                             });
-
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
@@ -280,13 +268,7 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                                 @Override
                                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                                     for (DataSnapshot datasnap : dataSnapshot.getChildren()) {
-                                        User u = null;
-                                        try {
-                                            u = User.buildUserObject(dataSnapshot);
-                                        } catch (NullPointerException e) {
-                                            Log.d(TAG, "NullPointerException is caught.");
-                                            e.printStackTrace();
-                                        }
+                                        User u = User.buildUserObject(dataSnapshot);
                                         if (u.getEmail().equals(username)) {
                                             u.addLockAttempts();
                                             u.writeToDatabase();
@@ -347,7 +329,6 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
                                 }
                             });
-
                         }
                     }
                 });
