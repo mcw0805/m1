a496b838ace53fdc3a8c8bfe498e53c2e42e3273
melaniehall12 <swimdawg@live.com>
2017-07-06 14:34:47 -0400

    Added splash screen

---
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/Splash_Screen.java
@@ -0,0 +1,30 @@
+package com.example.mcw0805.wheres_my_stuff.Controller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mcw0805.wheres_my_stuff.R;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2500);
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
