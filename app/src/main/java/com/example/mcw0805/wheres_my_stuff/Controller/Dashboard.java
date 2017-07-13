3c0e987c95447dc2384cc03d87bb54e5bf68b391
melaniehall12 <swimdawg@live.com>
2017-07-13 15:25:01 -0400

    Added vertical ellipse menu

--- app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/Dashboard.java
+++ app/src/main/java/com/example/mcw0805/wheres_my_stuff/Controller/Dashboard.java
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -164,6 +166,32 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
         top = Scene.getSceneForLayout((ViewGroup) mSceneRootTop, R.layout.activity_dashboard_top, this);
         //bottom = Scene.getSceneForLayout((ViewGroup)mSceneRootBottom, R.layout.activity_dashboard_bottom, this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.vertical_ellipse_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_profile) {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            Dashboard.this.startActivity(intent);
+        } else if (id == R.id.action_log_out) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            Dashboard.this.startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private SlidingUpPanelLayout.PanelSlideListener onSlideListener() {
