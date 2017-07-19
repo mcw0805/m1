cb4ed4bbe614c25aad1b0ab86ae78ad99d6729f4
melaniehall12 <swimdawg@live.com>
2017-07-19 13:03:12 -0400

    Added method contract/javadocs

--- app/src/test/java/com/example/mcw0805/wheres_my_stuff/Model/MelanieSwagTests.java
+++ app/src/test/java/com/example/mcw0805/wheres_my_stuff/Model/MelanieSwagTests.java
@@ -10,12 +10,19 @@ import java.util.List;
 
 /**
  * @author Melanie Hall
- *         <p>
- *         Testing {@code filterByActive} method of {@code UserListController}
+ * Testing {@code filterByActive} method of {@code UserListController}
  */
 
 public class MelanieSwagTests {
 
+    /*
+    * This test checks whether there's an empty list of Users.
+    * This test uses two Lists: an input list of zero Users and
+    * an expected list of zero Users.
+    * As the method runs, no Users should be added to either lists.
+    * The method should be able to check that both lists contain
+    * zero Users and should return true.
+     */
     @Test
     public void emptyArrayTest() {
         List<User> input = new ArrayList<>();
@@ -23,6 +30,14 @@ public class MelanieSwagTests {
         assertExpectedResult(input, expected);
     }
 
+    /*
+    * This test checks for all active Users in a list of Users.
+    * This test uses two Lists: an input list of two mock Users and
+    * an expected list of two mock Users.
+    * As the method runs, two mock Users should be added to both lists.
+    * The method should be able to check that both lists contain
+    * two mock Users and should return true.
+     */
     @Test
     public void allActiveUsersTest() {
         List<User> input = new ArrayList<>();
@@ -34,6 +49,15 @@ public class MelanieSwagTests {
         assertExpectedResult(input, expected);
     }
 
+    /*
+    * This test checks a list of Users when there are some banned Users.
+    * This test uses two Lists: an input list of two mock Users, one that
+    * has been banned, and an expected list of one mock User.
+    * As the method runs, two mock Users should be added to the input list, but one
+    * is set to true as banned; one User is added to the expected list.
+    * The method should be able to check that both lists contain
+    * one active User and should return true.
+     */
     @Test
     public void someActiveUsersTest() {
         List<User> input = new ArrayList<>();
@@ -46,6 +70,15 @@ public class MelanieSwagTests {
         assertExpectedResult(input, expected);
     }
 
+    /*
+    * This test checks a list of Users when there are no active Users.
+    * This test uses two Lists: an input list of two mock Users, both that
+    * have been banned, and an empty ArrayList of Users.
+    * As the method runs, two mock Users should be added to the input list, both
+    * are set to true as banned.
+    * The method should be able to check that both lists contain
+    * no active Users and should return true.
+     */
     @Test
     public void noActiveUsersTest() {
         List<User> input = new ArrayList<>();
@@ -57,6 +90,11 @@ public class MelanieSwagTests {
         assertExpectedResult(input, new ArrayList<User>());
     }
 
+    /*
+    * A helper method for assertEquals().
+    * @param input List of Users before running method
+    * @param expected List of Users that's expected
+     */
     private void assertExpectedResult(List<User> input, List<User> expected) {
         Assert.assertEquals(expected, UserListController.filterByActive(input));
     }
